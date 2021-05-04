6f4019037ccdf35356041a902a8343672a117b7f
Muhidin <muhidinhukic1@gmail.com>
2021-05-04 11:59:57 -0400

    scaffold views for organizations, shifts and added references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_153609) do
+ActiveRecord::Schema.define(version: 2021_05_04_155427) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.integer "hourly_rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "start"
+    t.datetime "finish"
+    t.integer "break_length"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shifts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +37,13 @@ ActiveRecord::Schema.define(version: 2021_05_04_153609) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "organization_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shifts", "users"
+  add_foreign_key "users", "organizations"
 end
