1b5845c6926c8ee6b89841dd7c49d9a6ff26f058
Muhidin <muhidinhukic1@gmail.com>
2021-05-05 17:00:38 -0400

    basic tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,24 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test test_truth do
-  #   assert true
-  # end
+  test "should not save user without name" do
+    user = User.new(email: "email@gmail.com", password: "123456")
+    assert_not user.save
+  end
+
+
+  test "should save user when all attributes present" do
+    user = User.new(email: "user@gmail.com", password: "123456", name: "Test User")
+    assert user.save
+  end
+
+
+  test "should encrypt user password" do
+    password = "123456"
+    user = User.new(email: "user@gmail.com", password: password, name: "Test User")
+    user.save
+    assert_not_equal(user.encrypted_password, "123456", ["Password Not Encrypted"])
+  end
+
 
 end
