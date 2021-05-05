d51c781e9b08020fda2a0ccceacc15d707174cee
Muhidin <muhidinhukic1@gmail.com>
2021-05-05 11:20:10 -0400

    added helper methods for displaying shift hours and selecting only current user organization shifts

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,2 +1,12 @@
 module ShiftsHelper
+
+#display only shifts related to current organization that user is in
+  def sanitize_shifts(shifts)
+    shifts.select {|shift| shift.user.organization == current_user.organization}
+  end
+
+  def convert_time(time)
+    time.strftime("%m/%d/%Y" + " at %I:%M%p")
+  end
+  
 end
