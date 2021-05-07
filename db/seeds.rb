5fd6d46e43406ba6de35be9bd4d08ddc7e83516a
Muhidin <muhidinhukic1@gmail.com>
2021-05-06 20:43:22 -0400

    seed.rb changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Organization.create("Muhidin's Burgers", hourly_rate: 22.5)
-Organization.create("Muhidin's Pizza", hourly_rate: 23)
-Organization.create("Workforce", hourly_rate: 25.8)
+Organization.create(name: "Muhidin's Burgers", hourly_rate: 22.5)
+Organization.create(name: "Muhidin's Pizza", hourly_rate: 23)
+Organization.create(name: "Workforce", hourly_rate: 25.9)
