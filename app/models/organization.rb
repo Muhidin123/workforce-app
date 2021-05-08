a8c398e3d6a11b97e91fbd3351fa3933be5a32f1
Muhidin <muhidinhukic1@gmail.com>
2021-05-08 14:43:48 -0400

    validation for name uniq for organizations, changed views

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,7 @@ class Organization < ApplicationRecord
     has_many :shifts, through: :users
 
     validates :name, :hourly_rate, presence: true
+    validates :name, uniqueness: true
 
     #will stop the user of submitting value less than one
     validates :hourly_rate, numericality: {greater_than: 0}
